5575fe3d20625199a57adabfd12e75034789cd5f
satyaprakash55 <119101506+satyaprakash55@users.noreply.github.com>
2022-12-12 22:13:32 +0530

    Add files via upload

---
+++ week-2-python-cipher-school/week2-Pythen-Cipherschools-main/defaultparameter.py
@@ -0,0 +1,4 @@
+def userid(username="satish38027",name,age):
+    print(f"your username{username} and name is {name} age is {age}")
+userid("satish141017","kamlesh",20)
+# error dekho kyu aa rahi hai
